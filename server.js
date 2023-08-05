42eb2ffbbe0415a6449b45a3abccb3647bbe7aec
Nishchal <nischal.15gupta@gmail.com>
2023-08-05 14:23:49 +0530

    face-detection-api

---
+++ server.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      //port : 3001,
+      user : 'postgres',
+      password : 'test',
+      database : 'smart_brain'
+    }
+  });
+
+  //the below query returns a promise
+//   db.select('*').from('users').then(data => {
+//     console.log(data);
+//   });
+
+const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.put('/image', (req, res) => {
+    const {id} = req.body;
+
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+})
+
+// --> '/profile/id' route that will render the user info on matching with their id
+app.get('/profile/:id', (req, res) => {
+
+    const {id} = req.params;
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if(user.length){
+            res.json(user[0])
+        }else{
+            res.status(400).json('Not found');
+        }
+    })
+    .catch(err => res.status(400).json('error getting user'));
+
+})
+//creating register route where a new user info will be added in our database 
+//from frontend
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            console.log('loginEMail is :', loginEmail[0].email);
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date() 
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+})
+
+//route route that displays all the users we have in our database
+app.get('/', (req, res) => {
+    res.send('success')
+});
+
+//creating signin route which will be a POST request
+app.post('/signin', (req, res) => {
+
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if(isValid){
+            return db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0]);
+            })
+            .catch(err => res.status(400).json('unable to get user'));
+        }else{
+            res.status(400).json('wrong credentials');
+        }
+    })
+    .catch(err => res.status(400).json('wrongg credentials')); 
+})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
